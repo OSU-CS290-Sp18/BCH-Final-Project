7164de3fcb1f6776d6911ce49c44a5618d4f0cea
cypertb <32154868+cypertb@users.noreply.github.com>
2018-06-14 02:26:59 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ var exphbs  = require('express-handlebars');
 var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
 
-var mongoHost = 'classmongo.engr.oregonstate.edu';
-var mongoPort = '27017';
-var mongoUsername = 'cs290_hoskinsc';
-var mongoPassword = 'cs290_hoskinsc';
-var mongoDBName = 'cs290_hoskinsc';
+var mongoHost = process.env.MONGO_HOST;
+var mongoPort = process.env.MONGO_PORT || '27017';
+var mongoUsername = process.env.MONGO_USERNAME;
+var mongoPassword = process.env.MONGO_PASSWORD;
+var mongoDBName = process.env.MONGO_DB_NAME;
 
 var mongoURL = "mongodb://" + mongoUsername + ":" + mongoPassword + "@" + mongoHost + ":" + mongoPort + "/" + mongoDBName;
 console.log(mongoURL);
